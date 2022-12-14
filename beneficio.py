15c4baa8ad9a0f8bdd4bfb832be78d57b7f7b9a1
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2022-12-14 12:40:38 -0500

    Agregar atributo 'interes_beneficio'
    El atributo tiene el valor por defecto de 0.05 (5%).
    
    El atributo es usado en el método mostrar y se usa para agregar un 5% de
    beneficio si el usuario es válido para este.

--- beneficio.py
+++ beneficio.py
@@ -2,15 +2,18 @@ from cuenta import Cuenta
 
 
 class Beneficio(Cuenta):
+
+    interes_beneficio: float = 0.05
+
     def __init__(self, nombre, apellido, cedula, edad, dinero) -> None:
         super().__init__(nombre, apellido, cedula, edad, dinero)
-        
+
     def es_usuario_valido(self) -> bool:
         return 18 <= self._edad < 28
 
     def mostrar(self):
         dinero_auxiliar = self._dinero_ahorrado
         if self.es_usuario_valido():
-            self._dinero_ahorrado *= 1.05
+            self._dinero_ahorrado *= 1 + self.interes_beneficio
         super().mostrar()
         self._dinero_ahorrado = dinero_auxiliar
